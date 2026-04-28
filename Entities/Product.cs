547a1ce9f90fe5f4b437e400ecc8595761c2ea4d
Felipe Lima <felipehenl122@gmail.com>
2026-04-27 21:41:21 -0300

    feat: initial structure with encapsulation and console menu

---
+++ Entities/Product.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+namespace SimpleWarehouse.Entities
+{
+    class Product
+    {
+        private string _name; 
+        public int Quantity { get; private set; }
+        public double Price { get; private set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            _name = name;
+            Price = price;
+            Quantity = quantity;
+
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { if (value != null && value.Length > 3) _name = value; }
+        }
+
+        public void UpdatePrice(double newPrice)
+        {
+            if (newPrice > 0 ) Price = newPrice;
+
+        }
+
+        public double TotalValueInStock()
+        {
+            return Price * Quantity;
+        }
+
+        public void AddProducts (int quantity)
+        {
+            Quantity += quantity;
+
+        }
+
+        public void RemoveProducts(int quantity)
+        {
+            if (quantity <= Quantity)
+            {
+                Quantity -= quantity;
+            }
+            else { Console.WriteLine("Error: Insufficient stock!"); }
+            
+           
+        }
+
+        public override string ToString()
+        {
+            return $"Product: {Name} | Price: {Price.ToString("f2",CultureInfo.InvariantCulture)} | Stock: {Quantity} | Total Value: $ {TotalValueInStock().ToString("f2",CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+using SimpleWarehouse.Entities;
+
+namespace SimpleWarehouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p;
+            Console.WriteLine();
+            Console.WriteLine("Enter product name");
+            string name = (Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Enter the Price");
+            double price = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.WriteLine("Enter the quantity");
+            int quantity = int.Parse(Console.ReadLine());
+            p = new Product(name, price, quantity);
+
+            while (true)
+            {
+                Console.WriteLine("1 - Add Stock , 2 - Remove Stock, 3 - Update Price, 4 - Exit");
+                string option = Console.ReadLine();
+
+                if (option == "1")
+                {
+                    Console.Write("Enter the quantity to be added to stock: ");
+                    int qte = int.Parse(Console.ReadLine());
+                    p.AddProducts(qte);
+                    Console.WriteLine(p);
+                }
+                else if (option == "2")
+                {
+                    Console.Write("Enter the quantity to be removed from stock: ");
+                    int qte = int.Parse(Console.ReadLine());
+                    p.RemoveProducts(qte);
+                    Console.WriteLine(p);
+                }
+                else if (option == "3")
+                {
+                    Console.WriteLine("Enter the new price value.");
+                    double newprice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    p.UpdatePrice(newprice);
+                    Console.WriteLine(p);
+                }
+                else 
+                {
+                    break;
+                }
+
+            }
+
+
+
+        }
+
+    }
+}
